074ebce6d3ad7f211831befe93d578befb232111
Jaakko <jiisala@gmail.com>
2022-07-15 13:25:02 +0300

    added propTypes for Blog component

--- Osa5/bloglist-frontend/src/components/Blog.js
+++ Osa5/bloglist-frontend/src/components/Blog.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const Blog = ({ blog, addLike, deleteBlog, userName }) => {
   const [visible, setVisible] = useState(false)
@@ -28,5 +29,19 @@ const Blog = ({ blog, addLike, deleteBlog, userName }) => {
     </div>
   )
 }
-
+Blog.prototypes = {
+  blog: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    user: PropTypes.shape({
+      username: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  }).isRequired,
+  addLike: PropTypes.func.isRequired,
+  deleteBlog: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired
+}
 export default Blog
